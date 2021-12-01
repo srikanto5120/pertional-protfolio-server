6208427461676ea12ee0a8aad7f17fc8112dbe2c
srikanto5120 <shreekantaray@gmail.com>
2021-12-01 15:04:07 +0600

    commit

--- index.js
+++ index.js
@@ -46,40 +46,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.post("/users", async (req, res) => {
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "shreekantaray@gmail.com",
-      pass: "{#$k/a/n/t/o<SK>5.1.2.0$#}",
-    },
-  });
-  var mailOptions = {
-    from: " shreekantaray@gmail.com ",
-    to: req.body.to,
-    subject: req.body.subject,
-    text: req.body.description,
-    html: `
-    <div style="padding:10px, border-style:ridge">
-        <p>You have a new connection</p>
-        <h3>Contact Details</h3>
-        <ul>
-            <li>Email:${req.body.to}</li>
-            <li>Subject:${req.body.subject}</li>
-            <li>Description:${req.body.description}</li>
-        </ul>
-        
-    </div>`,
-  };
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      res.json({ status: true, respMesg: "Email Sent Successfully" });
-    } else {
-      res.json({ status: true, respMesg: "Email Sent Successfully" });
-    }
-  });
-});
-
 app.get("/", (req, res) => {
   res.json("hello world");
 });
